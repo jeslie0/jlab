8de985120bde0d90e65ca66cc93db9e99f19aad4
James Leslie <jamesleslie@posteo.net>
2025-03-04 20:22:52 +0000

    Initial commit

---
+++ build-site.el
@@ -0,0 +1,81 @@
+;; Set the package installation directory so that packages aren't stored in the
+;; ~/.emacs.d/elpa path.
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+(package-install 'org-ref)
+(package-install 'org-roam)
+(require 'ox-publish)
+(require 'org-ref)
+(require 'org-roam)
+
+
+;; Very simplified version of org-ref-export-to from org-ref-export.el
+;; that export to filename
+(defun org-ref-export-to-file-nomarks-noopen
+    (backend filename &optional async subtreep visible-only body-only info)
+    (org-export-with-buffer-copy
+     (org-export-expand-include-keyword)
+     (org-ref-process-buffer backend subtreep)
+     (org-export-to-file backend filename
+             async subtreep visible-only
+             body-only info)
+     ))
+
+;; org-html-publish-to-html from ox-html.el adapted to org-ref
+;; Instead of org-export-to-file calls org-ref-export-to-file-nomarks-noopen
+(defun org-ref-html-publish-to-html (plist filename pub-dir)
+  (unless (or (not pub-dir) (file-exists-p pub-dir)) (make-directory pub-dir t))
+  ;; Check if a buffer visiting FILENAME is already open.
+  (let* ((org-inhibit-startup t)
+     (visiting (find-buffer-visiting filename))
+     (work-buffer (or visiting (find-file-noselect filename))))
+    (unwind-protect
+      (with-current-buffer work-buffer
+        (let ((output (org-export-output-file-name ".html" nil pub-dir)))
+          (org-ref-export-to-file-nomarks-noopen 'html output
+            nil nil nil (plist-get plist :body-only)
+            (org-combine-plists
+             plist
+             `(:crossrefs
+               ,(org-publish-cache-get-file-property
+                 ;; Normalize file names in cache.
+                 (file-truename filename) :crossrefs nil t)
+               :filter-final-output
+               (org-publish--store-crossrefs
+                org-publish-collect-index
+                ,@(plist-get plist :filter-final-output))))))))))
+
+
+
+(setq org-publish-project-alist
+      (list (list "jlab"
+                  :recursive t
+                  :base-directory "content"
+                  :publishing-directory "./public"
+                  :publishing-function 'org-ref-html-publish-to-html
+                  :with-author nil
+                  :with-creator nil
+                  :with-toc t
+                  :section-numbers t
+                  :time-stamp-file nil
+                  )))
+
+(setq org-html-validation-link nil
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
+      org-html-with-latex 'html
+      bibtex-completion-bibliography "~/texmf/bibtex/bib/bibliography.bib"
+      org-latex-to-html-convert-command (format "%s %s" (dired-make-absolute "latex-template.sh") "%i")
+      org-html-prefer-user-labels t)
+
+(setq org-html-head (mapconcat 'identity '("<link rel='stylesheet' href='/assets/css/nlab.css'></link>") "\n"))
+
+(org-publish-all t)
---
+++ latex-template.el
@@ -0,0 +1,38 @@
+(defun insert-latex-snippet (snippet)
+  (let ((template  "\\documentclass{article}
+\\usepackage[pdftex,active,tightpage]{preview}
+\\begin{document}
+\\begin{preview}
+%s
+\\end{preview}
+\\end{document}"))
+    (format template snippet)))
+
+(defun latex-to-svg (latex-code)
+  (let* ((tmp-dir "/tmp/texfrag/")
+         (hash (md5 latex-code))
+         (tex-file (concat tmp-dir hash ".tex"))
+         (dvi-file (concat tmp-dir hash ".dvi"))
+         (svg-file (concat tmp-dir hash ".svg")))
+    (if (file-exists-p svg-file)
+        (with-temp-buffer
+          (insert-file-contents svg-file)
+          (write-region (buffer-string) nil "/dev/stdout"))
+      (unless (file-directory-p tmp-dir)
+        (make-directory tmp-dir t))
+      (with-temp-file tex-file
+        (insert latex-code))
+      (call-process "lualatex" nil nil nil
+                    "--interaction=nonstopmode"
+                    "--shell-escape"
+                    "--output-format=dvi"
+                    (concat "--output-directory=" tmp-dir)
+                    tex-file)
+      (call-process "dvisvgm" nil nil nil
+                    dvi-file "-n" "-b" "min" "-c" "1.5" "-o" svg-file)
+      (with-temp-buffer
+                 (insert-file-contents svg-file)
+                 (write-region (buffer-string) nil "/dev/stdout")))))
+
+(let ((snippet (car command-line-args-left)))
+  (latex-to-svg ( insert-latex-snippet snippet)))
